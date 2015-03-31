6c3622d4735a6463ffbd8e98c7139f7e07acf232
abhinav1088 <azulkant@asu.edu>
2015-03-31 13:22:56 -0700

    Measurement.c  main file

--- MeasurementofDistance/MeasurementofDistance.c
+++ MeasurementofDistance/MeasurementofDistance.c
@@ -39,7 +39,7 @@ unsigned long Convert(unsigned long sample){
 
 // Initialize SysTick interrupts to trigger at 40 Hz, 25 ms
 void SysTick_Init(unsigned long period){
-	NVIC_ST_CTRL_R = 0;           // disable SysTick during setup
+	NVIC_ST_CTRL_R = 0;             // disable SysTick during setup
   NVIC_ST_RELOAD_R = period;		// reload value (80Mhz/40Hz)-1
   NVIC_ST_CURRENT_R = 0;        // any write to current clears it
   NVIC_SYS_PRI3_R = (NVIC_SYS_PRI3_R&0x00FFFFFF)|0x20000000; // priority 1
